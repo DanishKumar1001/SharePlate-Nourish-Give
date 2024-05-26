9b326c87dc99c5f39c9f4b2c00b73ee0b136ad42
danish <s223629209@deakin.edu.au>
2024-05-26 23:55:25 +1000

    Database connection error fixed

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const db = "mongodb+srv://danish:Danish123@cluster0.spb9vue.mongodb.net/";
+    // const db = "mongodb://localhost:27017";
     await mongoose.connect(db);
     console.log("Cloud MongoDB Cluster connected...");
   } catch (err) {
